842a3b3fc38f523bf88dadc4551ae059c10d8651
SID12g <sid12g@naver.com>
2024-08-13 22:37:50 +0900

    feat: comming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import Image from "next/image";
 import styles from "./page.module.css";
+import pickle from "@/../public/pickle.png";
 
 export default function Home() {
-  return <main></main>;
+  function onClickHandler() {
+    alert("Comming Soon!");
+  }
+  return (
+    <main className={styles.main}>
+      <Image
+        onClick={onClickHandler}
+        className={styles.image}
+        src={pickle}
+        alt="pickle"
+      />
+      <h1 className={styles.text}>Pickle Comming Soon.</h1>
+    </main>
+  );
 }
